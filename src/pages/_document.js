e9252435a039a8e76685f370863ee9af26236e62
Gabriel Gomes de Almeida <gabrielda@.ciandt.com>
2022-01-29 20:54:17 -0300

    SEO and Link Description Fixes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,16 +32,16 @@ export default class MyDocument extends Document {
       <Html lang='en-GB'>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-          <meta name="title" content="Vipul Jha - Android & Flutter Dev" />
-          <meta name="description" content="Hey there, This is Vipul Jha aka lordarcadius. I am an Android & Flutter developer who also works on ROMs, Kernels, & Scripts." />
+          <meta name="title" content="Almeida Dev - Web Full Stack Developer" />
+          <meta name="description" content="I am Gabriel Almeida, also known as Almeida. I am an Web Full Stack Developer who also works on Scripts and some Automation." />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Vipul Jha - Android & Flutter Dev" />
-          <meta property="og:description" content="Hey there, This is Vipul Jha aka lordarcadius. I am an Android & Flutter developer who also works on ROMs, Kernels, & Scripts." />
-          <meta property="og:image" content="https://www.vipuljha.com/images/banner.png" />
+          <meta property="og:title" content="Almeida Dev - Web Full Stack Developer" />
+          <meta property="og:description" content="I am Gabriel Almeida, also known as Almeida. I am an Web Full Stack Developer who also works on Scripts and some Automation." />
+          <meta property="og:image" content="https://www.almeidadeveloper.com/images/banner.png" />
           <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:title" content="Vipul Jha - Android & Flutter Dev" />
-          <meta property="twitter:description" content="Hey there, This is Vipul Jha aka lordarcadius. I am an Android & Flutter developer who also works on ROMs, Kernels, & Scripts." />
-          <meta property="twitter:image" content="https://www.vipuljha.com/images/banner.png" />
+          <meta property="twitter:title" content="Almeida Dev - Web Full Stack Developer" />
+          <meta property="twitter:description" content="I am Gabriel Almeida, also known as Almeida. I am an Web Full Stack Developer who also works on Scripts and some Automation." />
+          <meta property="twitter:image" content="https://www.almeidadeveloper.com/images/banner.png" />
         </Head>
         <body>
           <Main />
