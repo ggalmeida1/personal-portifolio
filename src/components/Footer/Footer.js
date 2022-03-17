48877720fd95aa000d644b0cc1522f6796395f94
Gabriel Gomes de Almeida <gabrielda@.ciandt.com>
2022-03-17 17:28:37 -0300

    Updated Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
       </LinkList>
       <SocialIconsContainer>
         <CompanyContainer>
-          <Slogan>&copy; 2021 <Link href="https://www.almeidadeveloper.com">  Almeida Dev</Link> . All rights reserved. </Slogan>
+          <Slogan>&copy; 2021 <Link href="https://www.almeidadeveloper.com">  Almeida Dev</Link>. All rights reserved. </Slogan>
           <br />
           <Slogan> Do you want to see the code for this website?  <LinkItem href="https://https://github.com/ggalmeida1/personal-portifolio">Check it here.</LinkItem></Slogan>
         </CompanyContainer>
